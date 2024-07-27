5800defdf7ab8a20e91fd65e1cdf794f437beb1b
Aman Kumari <amanag7605213@gmail.com>
2024-07-27 17:35:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home/>
+    </>
+
+  );
+};
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+      <div className="container home">
+        <div className="left">
+          <h1>Aman</h1>
+          <a href="" className="btn btn-outline-success">Download Resume</a>
+        </div>
+        <div className="right">
+
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
