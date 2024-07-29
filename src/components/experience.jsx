e8c3966465e4b0e8807d1e9f79f39ac397e880e6
Aman Kumari <amanag7605213@gmail.com>
2024-07-29 17:01:14 +0530

    experience responsive

--- src/components/experience.jsx
+++ src/components/experience.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import experience from './data/experience.json';
 
 const Experience = () => {
@@ -6,31 +6,27 @@ const Experience = () => {
     <>
       <div className="container ex" id="experience">
         <h1>Experience</h1>
-        {
-          experience.map((data) => {
-            return (
-              <>
-                <div key={data.id} className="ex-items text-center my-5"
-                  data-aos="zoom-in"
-                  data-aos-duration="1000"
-                >
-                  <div className="left">
-                    <img src={`/assets/${data.imageSrc}`} alt="" />
-                  </div>
-                  <div className="right">
-                    <h2>{data.role}</h2>
-                    <h4>
-                      <span style={{ color: '#a8326f' }}>{data.startDate}{"   "}{data.endDate}</span>{"   "}
-                      <span style={{ color: '#cc1641' }}>{data.location}</span>
-                    </h4>
-                    <h5 style={{ color: '#e876b1' }}>{data.experiences[0]}</h5>
-                    <h5 style={{ color: '#e876b1' }}>{data.experiences[1]}</h5>
-                  </div>
-                </div>
-              </>
-            )
-          })
-        }
+        {experience.map((data) => (
+          <div 
+            key={data.id} 
+            className="ex-items text-center my-5"
+            data-aos="zoom-in"
+            data-aos-duration="1000"
+          >
+            <div className="left">
+              <img src={`/assets/${data.imageSrc}`} alt={data.role} />
+            </div>
+            <div className="right">
+              <h2>{data.role}</h2>
+              <h4>
+                <span style={{ color: '#a8326f' }}>{data.startDate}{"   "}{data.endDate}</span>{"   "}
+                <span style={{ color: '#cc1641' }}>{data.location}</span>
+              </h4>
+              <h5 style={{ color: '#e876b1' }}>{data.experiences[0]}</h5>
+              <h5 style={{ color: '#e876b1' }}>{data.experiences[1]}</h5>
+            </div>
+          </div>
+        ))}
       </div>
     </>
   );
